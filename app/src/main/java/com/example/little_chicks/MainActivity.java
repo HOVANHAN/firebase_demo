06382cc23a5842201a322ca524bb8f740e437e40
hanha <hanhamho963@gmail.com>
2023-03-27 10:55:40 +0700

    retrieve data

--- app/src/main/java/com/example/little_chicks/MainActivity.java
+++ app/src/main/java/com/example/little_chicks/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,11 +17,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     Button btnAdd;
-    TextView txtData;
+    EditText txtData;
     private ListView listview;
 
     @Override
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtData =findViewById(R.id.txtData);
         btnAdd =findViewById(R.id.btnAdd);
         listview =findViewById(R.id.listview);
         ArrayList<String> list = new ArrayList<>();
@@ -39,8 +40,24 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 list.clear();
                 for (DataSnapshot snapshot1 : snapshot.getChildren()){
-                    list.add(snapshot1.getValue().toString());
+                   // Products products = snapshot1.getValue(Products.class);
+                 //   String txt = products.getName()+" "+products.getColor();
+                 //   Shops shop = snapshot1.getValue(Shops.class);
+                   // String txt = shop.getName() +" "+shop.getAdr();
+                  //  list.add(txt);
+                    for(DataSnapshot snapshot2 : snapshot1.getChildren()){
+                        for (DataSnapshot snapshot3 : snapshot2.getChildren())
+                        {
+                            list.add(snapshot3.getValue().toString());
+//                            for(int i = 0;i<list.size();i++)
+//                            {
+                                String[] product = list.get(0).split(",");
+                                txtData.setText(product[0].toString());
+//                            }
+                        }
+                    }
                 }
+
                 adapter.notifyDataSetChanged();
             }
 
